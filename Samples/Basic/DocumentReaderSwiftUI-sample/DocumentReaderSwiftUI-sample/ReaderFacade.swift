93b1095aea7168d0b68f2551eafe3d1f8f605128
Dmitry Evglevsky <dm.evglevsky@gmail.com>
2022-09-21 17:00:01 +0300

    optional result for timeout action

--- Samples/Basic/DocumentReaderSwiftUI-sample/DocumentReaderSwiftUI-sample/ReaderFacade.swift
+++ Samples/Basic/DocumentReaderSwiftUI-sample/DocumentReaderSwiftUI-sample/ReaderFacade.swift
@@ -129,11 +129,11 @@ class ReaderFacade: ObservableObject {
     }
     
     private func prepareCameraController() -> (controller: UIViewController,
-                                               results: AnyPublisher<DocumentReaderResults, Error>) {
+                                               results: AnyPublisher<DocumentReaderResults?, Error>) {
         var controller: UIViewController?
         let dismiss = { controller?.dismiss(animated: true) }
         
-        let future = Future<(DocumentReaderResults), Error> { promise in
+        let future = Future<(DocumentReaderResults)?, Error> { promise in
             
             controller = DocReader.shared.prepareCameraViewController { action, result, error in
                 switch action {
@@ -142,6 +142,10 @@ class ReaderFacade: ObservableObject {
                     dismiss()
                 case .error:
                     promise(.failure(error!))
+                    dismiss()
+                case .processTimeout:
+                    print("Timeout reached")
+                    promise(.success(nil))
                     dismiss()
                 default:
                     break
